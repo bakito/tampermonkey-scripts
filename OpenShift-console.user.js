6dd7b3812244f9e0bb12143f4b3e64e3aae4c47d
bakito <github@bakito.ch>
2018-04-25 08:17:04 +0200

    copy all props

--- OpenShift-console.user.js
+++ OpenShift-console.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OpenShift Console
 // @namespace    https://github.com/bakito
-// @version      0.3
+// @version      0.4
 // @description  add an additional search field to the OpenShift console
 // @run-at       document-end
 // @author       bakito
@@ -11,58 +11,69 @@
 // @updateURL    https://raw.githubusercontent.com/bakito/tampermonkey-scripts/master/OpenShift-console.user.js
 // @downloadURL  https://raw.githubusercontent.com/bakito/tampermonkey-scripts/master/OpenShift-console.user.js
 // ==/UserScript==
-var myObserver = new MutationObserver(mutationHandler);
-var obsConfig = {
-    childList: true, attributes: true,
-    subtree: true, attributeFilter: ['class']
-};
+(function () {
+    'use strict';
+    var myObserver = new MutationObserver(mutationHandler);
+    var obsConfig = {
+        childList: true, attributes: true,
+        subtree: true, attributeFilter: ['class']
+    };
 
-myObserver.observe(document, obsConfig);
+    myObserver.observe(document, obsConfig);
 
 
-function mutationHandler(mutationRecords) {
+    function mutationHandler(mutationRecords) {
 
-    mutationRecords.forEach(function (mutation) {
-        if (document.getElementById("openshift-custom-search") == null) {
-            if (mutation.type === "childList" && typeof mutation.addedNodes === "object" && mutation.addedNodes.length
-            ) {
-                for (var J = 0, L = mutation.addedNodes.length; J < L; ++J) {
-                    addNewSearchField(mutation.addedNodes[J]);
+        mutationRecords.forEach(function (mutation) {
+            if (document.getElementById("openshift-custom-search") == null) {
+                if (mutation.type === "childList" && typeof mutation.addedNodes === "object" && mutation.addedNodes.length
+                ) {
+                    for (var J = 0, L = mutation.addedNodes.length; J < L; ++J) {
+                        addNewSearchField(mutation.addedNodes[J]);
+                    }
+                }
+                else if (mutation.type === "attributes") {
+                    addNewSearchField(mutation.target);
                 }
             }
-            else if (mutation.type === "attributes") {
-                addNewSearchField(mutation.target);
+        });
+    }
+
+    function addNewSearchField(node) {
+        //-- Only process element nodes
+        if (node.nodeType === 1) {
+            if (node.id === 'search-projects') {
+
+                var newInput = document.createElement("input");
+                // copy all attributes
+                cloneAttributes(node, newInput);
+                newInput.id = "openshift-custom-search";
+                newInput.style = "margin-top: 17px;";
+                newInput.autofocus = true;
+                newInput.oninput = function () {
+                    var search = document.getElementById("search-projects");
+                    search.value = newInput.value;
+                    var event = new Event('change');
+                    search.dispatchEvent(event);
+                };
+                var newSearchDiv = document.createElement("div");
+                newSearchDiv.class = "navbar-header";
+                newSearchDiv.appendChild(newInput);
+
+                var header = document.getElementsByClassName("navbar-header")[0];
+
+                header.parentNode.insertBefore(newSearchDiv, header.nextSibling);
+
             }
         }
-    });
-}
+    }
 
-function addNewSearchField(node) {
-    //-- Only process element nodes
-    if (node.nodeType === 1) {
-        if (node.id === 'search-projects') {
 
-            var mi = document.createElement("input");
-            mi.id = "openshift-custom-search";
-            mi.style = "margin-top: 17px;";
-            mi.class = node.class;
-            mi.type = node.type;
-            mi.placeholder = node.placeholder;
-            mi.autofocus = true;
-            mi.oninput = function () {
-                var search = document.getElementById("search-projects");
-                search.value = mi.value;
-                var event = new Event('change');
-                search.dispatchEvent(event);
-            };
-            var mDiv = document.createElement("div");
-            mDiv.class = "navbar-header";
-            mDiv.appendChild(mi);
-
-            var header = document.getElementsByClassName("navbar-header")[0];
-
-            header.parentNode.insertBefore(mDiv, header.nextSibling);
-
+    function cloneAttributes(sourceNode, element) {
+        let attr;
+        let attributes = Array.prototype.slice.call(sourceNode.attributes);
+        while (attr = attributes.pop()) {
+            element.setAttribute(attr.nodeName, attr.nodeValue);
         }
     }
-}
+})();
